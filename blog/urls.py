358c6a4ce032d8d776fa4a74aaec4f6e8fff4695
Hamed <mohammadhamed1380@gmail.com>
2022-01-04 01:55:13 +0330

    set category parts

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import HomeView, PostView
+from .views import HomeView, PostView, CategoriesList, CategoryView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<slug:slug>/', PostView.as_view(), name='post'),
+    path('category/', CategoriesList.as_view(), name='category'),
+    path('category/<slug:slug>/', CategoryView.as_view(), name="categoryDetail"),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import get_object_or_404
 from django.views import generic
-from .models import Article
+from .models import Article, Category
 
 
 # Create your views here.
 
 class HomeView(generic.ListView):
     queryset = Article.objects.filter(status='p')
-    template_name = 'blog/index.html'
+    template_name = 'blog/home.html'
     context_object_name = 'articles'
     ordering = ['-publish']
     paginate_by = 2
@@ -20,3 +20,31 @@ class PostView(generic.DetailView):
 
     template_name = 'blog/post.html'
     context_object_name = 'article'
+
+
+class CategoriesList(generic.ListView):
+    template_name = 'blog/Categories.html'
+    queryset = Category.objects.filter(status=True)
+    context_object_name = 'categories'
+
+
+class CategoryView(generic.DetailView):
+    template_name = 'blog/Category_detail.html'
+
+    def get_object(self, queryset=None):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Category.objects.filter(status=True), slug=slug)
+
+    context_object_name = 'category'
+
+
+class CreateArticle(generic.CreateView):
+    template_name = "blog/create_article.html"
+
+
+class DeleteArticle(generic.DetailView):
+    template_name = "blog/delete_article.html"
+
+
+class UpdateArticle(generic.UpdateView):
+    template_name = "blog/update_article.html"
